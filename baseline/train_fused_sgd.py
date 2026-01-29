27c38d54488f7925e91b2b10d3f2e52e17e1e90e
jubsribs <julianacrispina@gmail.com>
2026-01-29 13:16:03 -0300

    adjusting imports SGDClassifier

--- baseline/train_fused_sgd.py
+++ baseline/train_fused_sgd.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from sklearn.ensemble import SGDClassifier
+from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import (
     accuracy_score,
